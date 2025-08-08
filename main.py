0f80405c9db7cf40cc2720a01526c0bbf662ba58
sofiandread <sofia@freshprints.com>
2025-08-08 22:40:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def static_files(filename):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     app.run(debug=False, host="0.0.0.0", port=port)
 
 
